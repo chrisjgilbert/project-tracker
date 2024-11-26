1b94bd8cc96aa6dc16636542aec62046d622333d
Chris Gilbert <chris@gosupercritical.com>
2024-11-26 21:35:10 +0000

    Add link to project from index

--- spec/system/view_projects_spec.rb
+++ spec/system/view_projects_spec.rb
@@ -10,4 +10,15 @@ feature "viewing all projects" do
     expect(page).to have_content "My first project"
     expect(page).to have_content "My second project"
   end
+
+  it "allows the user to view a project" do
+    create(:project, name: "My first project", status: :complete)
+
+    visit "/"
+
+    click_on "My first project"
+
+    expect(page).to have_content "My first project"
+    expect(page).to have_content "Complete"
+  end
 end
